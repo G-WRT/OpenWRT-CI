2ec0d3fd7b8c5f48db3ee0d30e8662c7a667a3af
G-WRT <hunk@motocam.net>
2026-03-31 21:04:09 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -65,6 +65,7 @@ UPDATE_PACKAGE "openwrt-bandix" "timsaya/openwrt-bandix" "main"
 UPDATE_PACKAGE "lucky" "sirpdboy/luci-app-lucky" "main"
 UPDATE_PACKAGE "luci-app-picoclaw" "GennKann/luci-app-picoclaw" "master"
 UPDATE_PACKAGE "picoclaw" "sipeed/picoclaw" "main"
+UPDATE_PACKAGE "openclaw" "10000ge10000/luci-app-openclaw" "main"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
